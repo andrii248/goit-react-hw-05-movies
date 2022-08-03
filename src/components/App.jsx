0f91505c147351fde503a649488f06ca077ea218
Andrii Krupa <krpaandrii@gmail.com>
2022-08-03 16:26:54 +0300

    API fetch data done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,15 @@
+import { getTrendingMovies } from 'services/moviesApi';
+import { searchMovies } from 'services/moviesApi';
+import { getMovieDetails } from 'services/moviesApi';
+import { getMovieCredits } from 'services/moviesApi';
+import { getMovieReviews } from 'services/moviesApi';
+
 export const App = () => {
+  // console.log(searchMovies('cat'));
+  // console.log(getMovieDetails('89125'));
+  // console.log(getMovieCredits('89125'));
+  // console.log(getMovieReviews('89125'));
+
   return (
     <div
       style={{
@@ -7,7 +18,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
